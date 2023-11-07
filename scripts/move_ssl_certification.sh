2d98f94f2db829e4b9fcfd9ad2e0d2b9e433a934
iksadNorth <iksadNorth@gmail.com>
2023-11-07 14:02:43 +0900

    "Feat : 인증서 이동"
    인증서 이동

---
+++ scripts/move_ssl_certification.sh
@@ -0,0 +1,13 @@
+domain_name=$DOMAIN_ADDRESS
+dir_archive='./certbot/conf/archive';
+
+sudo chmod 777 $dir_archive
+
+source_dir="$dir_archive/$domain_name"
+target_dir="$dir_archive/api.re-use.store"
+
+# 대상 디렉터리가 존재하지 않으면 생성합니다.
+mkdir -p "$target_dir"
+
+# 원본 디렉터리의 모든 파일을 대상 디렉터리로 이동합니다.
+mv "$source_dir"/* "$target_dir"
